7401c111c7516999e7aa3c2bb882e8b34f79ed06
GyeongChan <40288646+gclogs@users.noreply.github.com>
2022-12-14 14:24:56 +0000

    feat: implements mongoose connection for server

---
+++ backend/src/database.ts
@@ -0,0 +1,42 @@
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
+dotenv.config({ path: '.env' })
+
+let db: mongoose.Connection;
+
+class mongoDB {
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  constructor() {}
+  conn() {
+    if (db) return;
+    const { DATABASE_URL } = process.env;
+
+    mongoose.set("strictQuery", false);
+    
+    mongoose.Promise = global.Promise;
+    mongoose.connect(DATABASE_URL);
+
+    db = mongoose.connection;
+    
+    db.once('open', async () => {
+      console.log('Successfully connected to mongodb');
+    });
+
+    db.on('error', () => {
+      console.log(`Error connecting to databsae. Check Whether mongoDB
+      installed or you can try to give opensource Mongo Atlas database`);
+    });
+
+    return {}
+  }
+
+  disconn() {
+    if (!db) {
+      return;
+    }
+    mongoose.disconnect();
+  }
+}
+
+export default mongoDB;
