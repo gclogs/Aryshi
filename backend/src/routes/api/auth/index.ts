bea74aed09f046990d092ffc9676a9dfa03ad622
GyeongChan <40288646+gclogs@users.noreply.github.com>
2022-12-15 14:33:49 +0000

    feat: implements  local auth registeration for User

--- backend/src/routes/api/auth/index.ts
+++ backend/src/routes/api/auth/index.ts
@@ -1,10 +1,49 @@
 import Router from "@koa/router";
+import User from "../../../schema/user";
+import { passwordEncryption, userRegiseter } from '../../../services/user.service';
+
 const authRoute = new Router();
 
-authRoute.post('/register/local', )
+authRoute.post('/register/local', async ctx => {
+  const { name, email, nickname, password, service_number  } = ctx.request.body;
+  
+  const EncryptedPwd = passwordEncryption(password)
+  const user = userRegiseter(name, email, nickname, service_number, EncryptedPwd[0], EncryptedPwd[1]);
+  ctx.status = 200;
 
-authRoute.post('/login', )
+  console.log(user);
+})
 
-authRoute.post('/logout', )
+/** user manage */
+authRoute.get('/user', async ctx => {
+  const { id } = ctx.query;
+
+  try {
+    const user = await User.findById(id).exec();
+
+    if (!user) {
+      ctx.status = 404;
+      ctx.body = {
+        message: 'user not found'
+      }
+      return;
+    }
+
+    ctx.body = user;
+  } catch (e) {
+    console.log(e);
+  }
+})
+
+authRoute.delete('user/:id', async ctx => {
+  const id = ctx.params;
+
+  try {
+    await User.findByIdAndRemove(id).exec();
+    ctx.status = 200;
+  } catch (e) {
+    console.log(e);
+  }
+})
 
 export default authRoute;
