1a170f06a36af1708ddd6d5be59e41aa599d922c
GyeongChan <40288646+gclogs@users.noreply.github.com>
2022-12-19 14:18:54 +0000

    refactor: make services into an object

--- backend/src/schema/user.ts
+++ backend/src/schema/user.ts
@@ -1,13 +1,12 @@
 import { createSchema, Type, typedModel } from 'ts-mongoose';
 
 const UserSchema = createSchema({
-  user_id: Type.string({ required: true }),
-  name: Type.string({ required: true }),
   email: Type.string({ required: true, unique: true }),
-  nickname: Type.string(),
-  service_number: Type.number({ required: true}),
   password: Type.string({ required: true }),
-  salt: Type.string({ required: true })
+  salt: Type.string({required: true}),
+  name: Type.string({ required: true }),
+  createdAt: Type.date({ required: true}),
+  birth: Type.date()
 })
 
 const User = typedModel('User', UserSchema);
--- backend/src/services/user.service.ts
+++ backend/src/services/user.service.ts
@@ -1,29 +1,51 @@
 import { pbkdf2Sync, randomBytes } from "crypto"
+import db from '../lib/db'
 import User from "../schema/user";
 
-export const passwordEncryption = (password: string) => {
-  const salt = randomBytes(16).toString('base64');
-  const result: Array<string> = [pbkdf2Sync(password, salt, 10000, 64, 'SHA512').toString('base64'), salt]
-  return result;
+interface AuthParams {
+  email: string,
+  name: string,
+  password: string,
+  salt: any,
+  createdAt: string
+  birth: string
 }
 
-export const userRegiseter = async (
-  user_id: string,
-  name: string,
-  email: string,
-  nickname: string,
-  service_number: number,
-  password: string,
-  salt: string,
-) => {
-  const user = new User({
-    user_id, name, email, nickname, service_number, password, salt
-  });
-
+const hashedPassword = async (password: string) => {
+  const salt = await randomBytes(16).toString('base64');
   try {
-    await user.save();
-    return user;
+    const result: Array<string> = [pbkdf2Sync(password, salt, 9999, 64, 'SHA512').toString('base64'), salt]
+    return result;
   } catch (e) {
-    return e;
+    console.log(e);
+  }
+},
+
+const userService = {
+  async register({email, password, name, birth, salt, createdAt}: AuthParams) {
+    const exists = await db.user.findUnique({
+      where: {
+        email
+      }
+    })
+
+    if (exists) {
+      throw new Error('Aleady Email')
+    }
+
+    const hash: any = await hashedPassword(password);
+    const user = await db.user.create({
+      data: {
+        email,
+        password: hash[0],
+        name,
+        createdAt,
+        birth
+      }
+    })
+
+    return user;
   }
 }
+
+export default userService;
