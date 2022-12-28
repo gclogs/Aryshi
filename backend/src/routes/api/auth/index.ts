b69ce0d776b11e8347aae42b125241a7d4ea33a9
GyeongChan <40288646+gclogs@users.noreply.github.com>
2022-12-28 14:42:36 +0000

    refactor: make services into an object

--- backend/src/routes/api/auth/index.ts
+++ backend/src/routes/api/auth/index.ts
@@ -1,44 +1,59 @@
 import Router from "@koa/router";
-import User from "../../../schema/user";
 import userService from '../../../services/user.service'
 
 const authRoute = new Router();
 
 authRoute.post('/signup/local', async ctx => {
   const user = await userService.register(ctx.request.body);
-  if (user) {
-    ctx.status = 200;
-    return user;
-  }
+  
+  ctx.body = {
+    status: 200,
+    message: `${user.name} 회원이 가입하였습니다.`,
+    data: user
+  };
+
+  return user;
 })
 
-/** user manage */
 authRoute.get('/', async ctx => {
-  const { id } = ctx.query;
-
-  try {
-    const user = await User.findById(id).exec();
-
-    if (!user) {
-      ctx.status = 404;
-      ctx.body = {
-        message: 'user not found'
-      }
-      return;
-    }
-
-    ctx.body = user;
-  } catch (e) {
-    console.log(e);
-  }
+  const { email } = ctx.query;
+  const user = await userService.findEmail(email);
+  return user;
 })
 
-authRoute.delete('/:email', async ctx => {
-  const email  = ctx.params;
-  const authResult = await userService.unregister(email);
-  console.log(email);
-  ctx.status = 200;
-  return authResult;
+authRoute.put('/', async ctx => {
+  const user = await userService.update(ctx.request.body);
+
+  ctx.body = {
+    status: 200,
+    message: `${user.name}님이 ${ctx.request.body}을(를) 수정하였습니다.`,
+    data: user
+  }
+
+  return user;
+})
+
+authRoute.delete('/', async ctx => {
+  const { email }: any = ctx.query;
+  const user = await userService.unregister(email);
+
+  ctx.body = {
+    status: 200,
+    message: `${user.name}(${user.email}) 님이 회원탈퇴 하였습니다.`
+  }
+
+  return user;
+})
+
+authRoute.post('/login', async ctx => {
+  const user = await userService.login(ctx.request.body);
+
+  ctx.body = {
+    status: 200,
+    message: `${user.name} 님이 ${new Date()} 시간에 로그인 하였습니다.`,
+  };
+
+  return user;
 })
 
 export default authRoute;
