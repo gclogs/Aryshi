e3f955d6f744d21469a3b462db5993bae7dd7845
GyeongChan <40288646+gclogs@users.noreply.github.com>
2023-01-08 14:17:52 +0000

    delete: useless example code

--- backend/src/routes/api/auth/index.ts
+++ backend/src/routes/api/auth/index.ts
@@ -1,11 +1,7 @@
 import Router from "@koa/router";
 import authCtrl from "./auth.ctrl";
-import authMiddlewareAsync from '../../../middleware/authMiddleware'
-import db from "../../../lib/db";
 
 const authRoute = new Router();
-
-authRoute.get('/check', authMiddlewareAsync)
 
 authRoute.post('/signup/local', authCtrl.register);
 authRoute.post('/login', authCtrl.loign);
