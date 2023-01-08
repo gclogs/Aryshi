bed679327aea1e62122abc8b75642515a9b475ea
GyeongChan <40288646+gclogs@users.noreply.github.com>
2023-01-08 14:00:01 +0000

    feat: implments jwt middleware auth

--- backend/src/middleware/authMiddleware.ts
+++ backend/src/middleware/authMiddleware.ts
@@ -1,10 +1,7 @@
-import jwt from 'jsonwebtoken'
 import { Context } from "koa";
 import { validateToken } from "../lib/token";
 
-const { JsonWebTokenError } = jwt;
-
-export const authMiddlewareAsync = async (ctx: Context, next) => {
+const authMiddlewareAsync = async (ctx: Context, next) => {
   const token = ctx.request.header.authorization?.split('Bearer ')[1] ?? ctx.cookies.get('access_token');
   
   if (ctx.cookies.get('refresh_token') && !token) {
@@ -15,9 +12,10 @@ export const authMiddlewareAsync = async (ctx: Context, next) => {
 
   try {
     const decoded = await validateToken(token);
+    ctx.request.user = decoded;
   } catch (e: any) {
-    if (e instanceof JsonWebTokenError) {
-      console.log(e)
-    }
+    console.log(e)
   }
 }
+
+export default authMiddlewareAsync;
