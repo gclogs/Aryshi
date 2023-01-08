07ffe0831343913f227c613a72c4658f8df8d1b6
GyeongChan <40288646+gclogs@users.noreply.github.com>
2023-01-08 14:00:16 +0000

    refactor: make services into an object

---
+++ backend/src/routes/api/auth/auth.ctrl.ts
@@ -0,0 +1,49 @@
+import { Context } from "koa";
+import { clearCookies, setTokenCookie } from "../../../lib/cookies";
+import userService from "../../../services/user.service"
+
+const authCtrl = {
+  async loign(ctx: Context) {
+    const authResult = await userService.login(ctx.request.body);
+
+    setTokenCookie(ctx, 'access_token', authResult.tokens.accessToken);
+    setTokenCookie(ctx, 'refresh_token', authResult.tokens.refreshToken);
+
+
+    ctx.body = {
+      data: authResult
+    }
+
+    return authResult;
+  },
+
+  async logout(ctx: Context) {
+    ctx.request.user = null;
+    clearCookies(ctx);
+  },
+
+  async register(ctx: Context) {
+    const authResult = await userService.register(ctx.request.body);
+    return authResult;
+  },
+
+  async remove(ctx: Context) {
+    const authResult = await userService.unregister(ctx.request.body);
+    return authResult;
+  },
+
+  async refresh(ctx: Context) {
+    const refreshToken = ctx.request.requestToken ?? ctx.cookies.get('refresh_token');
+    if (!refreshToken) {
+      throw new Error('BadRequest');
+    }
+
+    const tokens = await userService.refreshToken(refreshToken);
+
+    setTokenCookie(ctx, 'access_token', authResult.tokens.accessToken);
+
+    return tokens;
+  }
+}
+
+export default authCtrl;
--- backend/src/routes/api/auth/index.ts
+++ backend/src/routes/api/auth/index.ts
@@ -1,52 +1,16 @@
 import Router from "@koa/router";
-import userService from '../../../services/user.service'
+import authCtrl from "./auth.ctrl";
+import authMiddlewareAsync from '../../../middleware/authMiddleware'
+import db from "../../../lib/db";
 
 const authRoute = new Router();
 
-authRoute.post('/signup/local', async ctx => {
-  const authResult = await userService.register(ctx.request.body);
-  
-  ctx.body = {
-    status: 200,
-    message: `${authResult.user.name} 회원이 가입하였습니다.`,
-    data: authResult.tokens
-  };
+authRoute.get('/check', authMiddlewareAsync)
 
-  return authResult;
-})
-
-authRoute.delete('/', async ctx => {
-  const { email }: any = ctx.query;
-  const user = await userService.unregister(email);
-
-  ctx.body = {
-    status: 200,
-    message: `${user.name}(${user.email}) 님이 회원탈퇴 하였습니다.`
-  }
-
-  return user;
-})
-
-authRoute.post('/login', async ctx => {
-  const authResult = await userService.login(ctx.request.body);
-
-  ctx.cookies.set('access_token', authResult.tokens.accessToken, {
-    httpOnly: true,
-    maxAge: 1000 * 60 * 60 * 24 * 7
-  });
-
-  ctx.cookies.set('refresh_token', authResult.tokens.refreshToken, {
-    httpOnly: true,
-    maxAge: 1000 * 60 * 60 * 24 * 7
-  });
-
-  console.log(ctx.cookies.get('refresh_token'))
-
-  if (authResult.user) {
-    ctx.body = authResult.user
-  }
-
-  return authResult;
-})
+authRoute.post('/signup/local', authCtrl.register);
+authRoute.post('/login', authCtrl.loign);
+authRoute.delete('/', authCtrl.remove);
+authRoute.post('/refresh', authCtrl.refresh);
+authRoute.post('/logout', authCtrl.logout);
 
 export default authRoute;
