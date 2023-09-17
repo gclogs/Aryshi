fac394d0a348f45a31bd708b5bff5a6d2355e37c
GyeongChan <woig9874@naver.com>
2023-09-18 04:01:47 +0900

    feat: design the root page

--- frontend/app/routes/index.tsx
+++ frontend/app/routes/index.tsx
@@ -1,15 +1,26 @@
-import { Link } from "@remix-run/react";
+import Header from "~/components/base/Header";
+
 
 export default function Index() {
+
   return (
-    <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.4' }}>
-      <h1 className="text-blue-500">Welcome to Remix</h1>
-      <h1 className='text-blue-500'>ㅇ응애 시발 드디어 tailwindcss가 먹네 ㅈ같은거</h1>
-      <ul>
-        <li>
-          <Link to="/auth/register">register</Link>
-        </li>
-      </ul>
-    </div>
+    <>
+      <Header />
+      <footer>
+        <div>
+          <ul>
+            <li>
+              <img src="assets/home_active.svg" alt="" />
+            </li>
+            <li>
+              <img src="assets/write_disable.svg" alt="" />
+            </li>
+            <li>
+              <img src="assets/bubble_disable.svg" alt="" />
+            </li>
+          </ul>
+        </div>
+      </footer>
+    </>
   );
 }
