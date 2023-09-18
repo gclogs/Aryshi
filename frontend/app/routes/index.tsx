e0aa430b8c384af02bfc5577ec7e71fd2ff5151b
GyeongChan <woig9874@naver.com>
2023-09-18 20:18:08 +0900

    feat: implementation the footer menu function

--- frontend/app/routes/index.tsx
+++ frontend/app/routes/index.tsx
@@ -1,26 +1,57 @@
+import { Link } from "@remix-run/react";
 import Header from "~/components/base/Header";
 
+interface ListProps {
+  list: Array<LinkedAssetsVectors>;
+}
+
+interface LinkedAssetsVectors {
+  vector: string;
+  link: string;
+  style?: string;
+}
 
 export default function Index() {
-
+  const assets = [
+    {vector: "home_disable", link: "/", style: "w-8"},
+    {vector: "write_disable", link: "/", style: "w-7"},
+    {vector: "bubble_disable", link: "/", style: "w-8"},
+    {vector: "profile_disable", link: "/", style: "w-6"},
+  ]
+  
   return (
-    <>
+    <> 
       <Header />
       <footer>
-        <div>
-          <ul>
-            <li>
-              <img src="assets/home_active.svg" alt="" />
-            </li>
-            <li>
-              <img src="assets/write_disable.svg" alt="" />
-            </li>
-            <li>
-              <img src="assets/bubble_disable.svg" alt="" />
-            </li>
-          </ul>
-        </div>
+        <Border>
+          <List list={assets}/>
+        </Border>
       </footer>
     </>
   );
+}
+
+const Border = ({children}) => {
+  return (
+    <div className="border-t-1 items-center">
+      {children}
+    </div>
+  )
+}
+
+const List = ({list}: ListProps) => {
+  return (
+    <ul className="flex justify-center">
+      {list.map((v) => (
+        <li className="px-5 py-3">
+          <Link to={v.link}>
+            <img 
+              className={`${v.style}`}
+              src={`/assets/${v.vector}.svg`}
+              />
+          </Link>
+        </li>
+      ))}
+    </ul>
+  )
 }
