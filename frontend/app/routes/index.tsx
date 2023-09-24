d5acefb80a21ad83e327b211b414ba9ab530ee75
GyeongChan <woig9874@naver.com>
2023-09-25 02:42:03 +0900

    refactor: home page testing

--- frontend/app/routes/index.tsx
+++ frontend/app/routes/index.tsx
@@ -1,57 +1,18 @@
-import { Link } from "@remix-run/react";
-import Header from "~/components/base/Header";
-
-interface ListProps {
-  list: Array<LinkedAssetsVectors>;
-}
-
-interface LinkedAssetsVectors {
-  vector: string;
-  link: string;
-  style?: string;
-}
+import Footer from "~/components/base/Footer";
 
 export default function Index() {
-  const assets = [
-    {vector: "home_disable", link: "/", style: "w-8"},
-    {vector: "write_disable", link: "/", style: "w-7"},
-    {vector: "bubble_disable", link: "/", style: "w-8"},
-    {vector: "profile_disable", link: "/", style: "w-6"},
-  ]
-  
   return (
-    <> 
-      <Header />
-      <footer>
-        <Border>
-          <List list={assets}/>
-        </Border>
-      </footer>
+    <>
+      <Content />
     </>
   );
 }
 
-const Border = ({children}) => {
+const Content = () => {
   return (
-    <div className="border-t-1 items-center">
-      {children}
-    </div>
+    <>
+        아니 이거 왜이럼??
+    </>
   )
 }
 
-const List = ({list}: ListProps) => {
-  return (
-    <ul className="flex justify-center">
-      {list.map((v) => (
-        <li className="px-5 py-3">
-          <Link to={v.link}>
-            <img 
-              className={`${v.style}`}
-              src={`/assets/${v.vector}.svg`}
-              />
-          </Link>
-        </li>
-      ))}
-    </ul>
-  )
-}
