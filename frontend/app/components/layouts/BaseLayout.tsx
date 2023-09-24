179d610d2ec1fd8eaf41d81c9f524da837cddf0e
GyeongChan <woig9874@naver.com>
2023-09-25 02:41:10 +0900

    fix: syntax error

--- frontend/app/components/layouts/BaseLayout.tsx
+++ frontend/app/components/layouts/BaseLayout.tsx
@@ -13,9 +13,6 @@ export default function BaseLayout({
   isHeaderVisible
 }: Props) {
   return (
-   <Block>
-      <Header title={title} />
-      <Content>{children}</Content>
-   </Block>
+    <Header title={title} />
   );
 }
